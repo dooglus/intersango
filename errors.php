8f978e223d5c54267e1f3c877e497616be178c39
genjix <fake@lol.u>
2011-03-12 21:51:32 +0000

    new error handling system. does reports and system logs.

---
+++ errors.php
@@ -0,0 +1,92 @@
+<?php
+
+class SEVERITY
+{
+    const PROBLEM = 0;
+    const ERROR = 1;
+    const BAD_PAGE = 2;
+}
+
+function report($message, $severity)
+{
+    $uid = '';
+    if (isset($_SESSION['uid']))
+        $uid = $_SESSION['uid'];
+    $time = time();
+    $message = "$uid $time: $message";
+
+    switch ($severity) {
+        case SEVERITY::PROBLEM:
+            $filename = "/var/tmp/problem-reports.log";
+            break;
+
+        case SEVERITY::ERROR:
+            $filename = "/var/tmp/error-reports.log";
+            break;
+
+        case SEVERITY::BAD_PAGE:
+            $filename = "/var/tmp/bad-page-reports.log";
+            break;
+
+        default:
+            report("Invalid report for $message of $severity!", SEVERITY::ERROR);
+            break;
+    }
+
+    error_log("$message\n", 3, $filename);
+    # do this last because it's the most risky operation, and we at least want some logs first.
+    if ($severity == SEVERITY::ERROR) {
+        echo exec("echo 'A fatal error has occured. Time is now $time.' | mutt -s INTERSANGO_ERROR genjix@gmail.com -a $filename");
+    }
+}
+function log_badpage($page)
+{
+    report($page, SEVERITY::BAD_PAGE);
+}
+function report_exception($e, $severity)
+{
+    $title = $e->getTitle();
+    $message = $e->getMessage();
+    report("Exception:\n==== $title ====\n$message\n================", $severity);
+}
+
+function reporting_error_handler($errno, $errstr, $errfile, $errline)
+{
+    if (!(error_reporting() & $errno)) {
+        # This error code is not included in error_reporting
+        return;
+    }
+    switch ($errno) {
+    case E_USER_ERROR:
+        report("[$errno] $errstr $errline in $errfile", SEVERITY::ERROR);
+        exit(1);
+        break;
+
+    case E_USER_WARNING:
+        report("WARNING: [$errno] $errstr", SEVERITY::ERROR);
+        break;
+
+    case E_USER_NOTICE:
+        report("NOTICE: [$errno] $errstr", SEVERITY::ERROR);
+        break;
+
+    default:
+        report("UNKNOWN: [$errno] $errstr", SEVERITY::ERROR);
+        break;
+    }
+    # Don't execute PHP internal error handler
+    return true;
+}                                
+function reporting_shutdown() { 
+    $error = error_get_last();
+    if ($error != NULL) {
+        $info = "[SHUTDOWN] file:".$error['file']." | ln:".$error['line']." | msg:".$error['message'] .PHP_EOL;
+        report($info, SEVERITY::ERROR);
+    } 
+}
+
+set_error_handler("reporting_error_handler");
+register_shutdown_function("reporting_shutdown");
+
+?>
+
--- switcher.php
+++ switcher.php
@@ -1,11 +1,7 @@
 <?php
 # security protection
 defined('_we_are_one') || die('Direct access not allowed.');
-
-function report()
-{
-    echo 'Reported';
-}
+require 'errors.php';
 
 function switcher($page, $loggedin)
 {
@@ -16,42 +12,51 @@ function switcher($page, $loggedin)
                 if ($loggedin)
                     include("$abspath/profile.php");
                 else
-                    report();
+                    log_badpage($page);
                 break;
+
             case 'login':
                 if (!$loggedin)
                     # first force logout
                     include("$abspath/login.php");
                 else
-                    report();
+                    log_badpage($page);
                 break;
+
             case 'deposit':
                 include("$abspath/deposit.php");
                 break;
+
             case 'withdraw':
                 include("$abspath/withdraw.php");
                 break;
+
             case 'help':
                 include("$abspath/help.php");
                 break;
+
             case 'orderbook':
                 include("$abspath/orderbook.php");
                 break;
+
             case 'place_order':
                 if ($loggedin)
                     include("$abspath/place_order.php");
                 else
-                    report();
+                    log_badpage($page);
                 break;
+
             case '':
                 include("$abspath/index.php");
                 break;  
+
             default:
-                report();
+                log_badpage($page);
                 break;
         }
     } 
     catch (Error $e) {
+        report_exception($e, SEVERITY::ERROR);
         # Same as below, but flag + log this for review,
         echo "<div class='content_box'><h3>{$e->getTitle()}</h3>";
         echo "<p>{$e->getMessage()}</p></div>";
