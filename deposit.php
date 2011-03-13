ecd836476fa71832372de1f04d7d79d61e4799ee
genjix <fake@lol.u>
2011-03-13 01:10:46 +0000

    added deposit info.

--- deposit.php
+++ deposit.php
@@ -1,14 +1,43 @@
+<?php
+require 'util.php';
+$uid = user_id();
+$bitcoin = connect_bitcoin();
+$addy = $bitcoin->getaccountaddress($uid);
+
+$query = "
+    SELECT deposref
+    FROM users
+    WHERE uid='$uid';
+";
+$result = do_query($query);
+$row = get_row($result);
+$deposref = $row['deposref'];
+?>
+
 <div class='content_box'>
     <h3>Deposit GBP</h3>
-    <p>Depositing is free by bank deposit for this month of March. Email sdlkssdsd for details.</p>
+    <p>Depositing is free by bank deposit for this month of March. You are responsible for paying any incurred fees. If your deposit is insufficient to cover bank fees then it be denied.</p>
+    <p>You will need to quote <?php echo $deposref; ?> in the transaction's reference field.</p>
+    <table class='display_data'>
+        <tr>
+            <td>Account title:</td>
+            <td>Mr Taaki</td>
+        </tr>
+        <tr>
+            <td>Account number:</td>
+            <td>22939560</td>
+        </tr>
+        <tr>
+            <td>Branch sort code:</td>
+            <td>30 96 93</td>
+        </tr>
+        <tr>
+            <td>Reference:</td>
+            <td><?php echo $deposref; ?></td>
+        </tr>
+    </table>
+    <p>Allow 3-5 working days for payments to pass through clearing.</p>
 </div>
-
-<?php
-    require 'util.php';
-    $uid = user_id();
-    $bitcoin = connect_bitcoin();
-    $addy = $bitcoin->getaccountaddress($uid);
-?>
 
 <div class='content_box'>
     <h3>Deposit BTC</h3>
