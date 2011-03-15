55ed0b148f2ceff73fb0b6318aef53c6a1a05063
genjix <fake@lol.u>
2011-03-15 10:24:07 +0000

    desc time + don't show transactions in order view when they've been cancelled.

--- view_util.php
+++ view_util.php
@@ -20,43 +20,47 @@ function display_transactions($uid, $orderid)
             transactions.a_orderid=orderbook.orderid
             OR transactions.b_orderid=orderbook.orderid
         WHERE orderbook.uid='$uid' $ordselq
-        ORDER BY transactions.timest ASC;
+        ORDER BY transactions.timest DESC;
     ";
     $result = do_query($query);
     $row = mysql_fetch_assoc($result);
-    if ($row) { ?>
-        <div class='content_box'>
-        <h3>Your trades <?php if ($orderid != -1) echo 'for this order'; ?></h3>
-        <table class='display_data'>
-            <tr>
-                <th>Description</th>
-                <th>Time</th>
-                <?php if ($orderid == -1) echo '<th></th>'; ?>
-            </tr><?php
-        do {
-            $who = $row['who'];
-            $a_amount = $row['a_amount'];
-            $b_amount = $row['b_amount'];
-            if ($who == 'A')
-                list($a_amount, $b_amount) = array($b_amount, $a_amount);
-            # skip cancelled orders since we already show those
-            if ($b_amount == -1)
-                continue;
-            $a_amount = internal_to_numstr($a_amount);
-            $b_amount = internal_to_numstr($b_amount);
-            $type = $row['type'];
-            $want_type = $row['want_type'];
-            $orderid = $row['orderid'];
-            $timest = $row['timest'];
-            echo "    <tr>\n";
-            echo "        <td>You gave $a_amount $type for <b>$b_amount $want_type</td>\n";
-            echo "        <td>$timest</td>\n";
-            if ($orderid == -1)
-                echo "        <td><a href='?page=view_order&orderid=$orderid'>View order</a></td>\n";
-            echo "    </tr>\n";
-        } while ($row = mysql_fetch_assoc($result));
-        echo "</table></div>";
+    $first = true;
+    while ($row = mysql_fetch_assoc($result)) {
+        $who = $row['who'];
+        $a_amount = $row['a_amount'];
+        $b_amount = $row['b_amount'];
+        if ($who == 'B')
+            list($a_amount, $b_amount) = array($b_amount, $a_amount);
+        # skip cancelled orders since we already show those
+        if ($b_amount == -1)
+            continue;
+        if ($first) {
+            $first = false;
+            ?> <div class='content_box'>
+            <h3>Your trades <?php if ($orderid != -1) echo 'for this order'; ?></h3>
+            <table class='display_data'>
+                <tr>
+                    <th>Description</th>
+                    <th>Time</th>
+                    <?php if ($orderid == -1) echo '<th></th>'; ?>
+                </tr><?php
+        }
+
+        $a_amount = internal_to_numstr($a_amount);
+        $b_amount = internal_to_numstr($b_amount);
+        $type = $row['type'];
+        $want_type = $row['want_type'];
+        $orderid = $row['orderid'];
+        $timest = $row['timest'];
+        echo "    <tr>\n";
+        echo "        <td>You gave $a_amount $type for <b>$b_amount $want_type</td>\n";
+        echo "        <td>$timest</td>\n";
+        if ($orderid == -1)
+            echo "        <td><a href='?page=view_order&orderid=$orderid'>View order</a></td>\n";
+        echo "    </tr>\n";
     }
+    if (!$first)
+        echo "</table></div>";
 }
 
 ?>
