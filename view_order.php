48caee8a6e976bfcc9e0a161beca4499904c790e
genjix <fake@lol.u>
2011-03-15 02:31:22 +0000

    use function to fetch record for order from fulfill_order.php

--- view_order.php
+++ view_order.php
@@ -1,46 +1,43 @@
 <?php
 require 'util.php';
+require 'fulfill_order.php';
 if (!isset($_GET['orderid']))
     throw new Problem('No order selected', 'Hit back and select an order.');
 $orderid = $_GET['orderid'];
+$uid = user_id();
+$info = fetch_order_info($orderid);
 
 if (isset($_POST['cancel_order'])) {
     # cancel an order
     $query = "
         UPDATE orderbook
         SET status='CANCEL'
-        WHERE orderid='$orderid';
+        WHERE
+            orderid='$orderid'
+            AND uid='$uid';
     ";
     do_query($query);
+
+    if ($uid != $info->uid)
+        throw new Error('Permission...', '... Denied! Now GTFO.');
+
+    add_funds($info->uid, $info->amount, $info->type);
+    # these records indicate returned funds.
+    create_record($orderid, $info->amount, -1, 0);
     ?><div class='content_box'>
         <h3>Cancelled!</h3>
         <p>Order <?php echo $orderid; ?> is no more.</p>
     </div><?php
 }
 else {
-    $query = "
-        SELECT
-            initial_amount,
-            amount,
-            type,
-            initial_want_amount,
-            want_amount,
-            want_type,
-            DATE_FORMAT(timest, '%H%i %d/%m/%y') AS timest,
-            status
-        FROM orderbook
-        WHERE orderid='$orderid';
-    ";
-    $result = do_query($query);
-    $row = get_row($result);
-    $initial_amount = internal_to_numstr($row['initial_amount']);
-    $amount = internal_to_numstr($row['amount']);
-    $type = $row['type'];
-    $initial_want_amount = internal_to_numstr($row['initial_want_amount']);
-    $want_amount = internal_to_numstr($row['want_amount']);
-    $want_type = $row['want_type'];
-    $timest = $row['timest'];
-    $status = $row['status'];
+    $initial_amount = internal_to_numstr($info->initial_amount);
+    $amount = internal_to_numstr($info->amount);
+    $type = $info->type;
+    $initial_want_amount = internal_to_numstr($info->initial_want_amount);
+    $want_amount = internal_to_numstr($info->want_amount);
+    $want_type = $info->want_type;
+    $timest = $info->timest;
+    $status = $info->status;
     ?> <div class='content_box'>
         <h3>Order info</h3>
         <p>
