636b17b273144920ee57099320069f476c70bd4b
genjix <fake@lol.u>
2011-03-24 21:10:29 +0000

    compares rates in sanitt check.

--- scr/sanity.sql
+++ scr/sanity.sql
@@ -1,13 +1,51 @@
-SELECT *, b_want/b_amount AS b_rate, a_amount/a_want AS a_rate
+SELECT
+    txid,
+    b_r AS b_rate,
+    a_r AS a_rate,
+    r AS rate,
+    b_r > a_r,
+    r > a_r,
+    r < b_r
 FROM (
-    SELECT txid, a_amount, a_want, initial_amount AS b_amount, initial_want_amount AS b_want
+    SELECT
+        txid,
+        b_want/b_amount AS b_r,
+        a_amount/a_want AS a_r,
+        a_exc/b_exc AS r
     FROM (
-        SELECT txid, initial_amount AS a_amount, initial_want_amount AS a_want, b_orderid
-        FROM transactions
-        JOIN orderbook
-        ON transactions.a_orderid=orderbook.orderid
-        ) AS a
-    JOIN orderbook
-    ON b_orderid=orderbook.orderid
-    ) AS j
-HAVING b_rate > a_rate;
+        SELECT
+            txid,
+            a_amount,
+            a_want,
+            initial_amount AS b_amount,
+            initial_want_amount AS b_want,
+            a_exc,
+            b_exc
+        FROM (
+            SELECT
+                t.txid,
+                o.initial_amount AS a_amount,
+                o.initial_want_amount AS a_want,
+                t.b_orderid,
+                t.a_amount AS a_exc,
+                t.b_amount AS b_exc
+            FROM
+                transactions AS t
+            JOIN
+                orderbook AS o
+            ON
+                t.a_orderid=o.orderid
+            WHERE
+                b_amount >= 0
+            ) AS a
+        JOIN
+            orderbook
+        ON
+            b_orderid=orderbook.orderid
+        ) AS j
+    ) AS t
+WHERE
+    b_r > a_r
+    OR r > a_r
+    OR r < b_r
+    OR TRUE
