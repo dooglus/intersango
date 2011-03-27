795edb6597e6ad6f6af5cb0946f1b248ff1d8ee0
genjix <fake@lol.u>
2011-03-28 00:38:12 +0100

    getDepth.php in API

---
+++ www/api/getDepth.php
@@ -0,0 +1,52 @@
+<?php
+require "../config.php";
+require "$abspath/util.php";
+
+$query = "
+    SELECT
+        initial_want_amount / initial_amount AS rate,
+        amount
+    FROM
+        orderbook
+    WHERE
+        type='BTC'
+        AND want_type='GBP'
+    ";
+$result = do_query($query);
+$first = true;
+echo '{"asks": [';
+while ($row = mysql_fetch_assoc($result)) {
+    if ($first)
+        $first = false;
+    else
+        echo ", ";
+    $rate = $row['rate'];
+    $amount = internal_to_numstr($row['amount']);
+    echo "[$rate, $amount]";
+}
+echo '], "bids": [';
+$query = "
+    SELECT
+        initial_amount / initial_want_amount AS rate,
+        amount
+    FROM
+        orderbook
+    WHERE
+        type='GBP'
+        AND want_type='BTC'
+    ";
+$result = do_query($query);
+$first = true;
+while ($row = mysql_fetch_assoc($result)) {
+    if ($first)
+        $first = false;
+    else
+        echo ", ";
+    $rate = $row['rate'];
+    $amount = internal_to_numstr($row['amount']);
+    echo "[$rate, $amount]";
+}
+echo ']}';
+
+?>
+
