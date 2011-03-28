ca1cf94abd629c9a6e180f6a117e03981dd05d55
genjix <fake@lol.u>
2011-03-28 22:54:55 +0100

    take command line argument.

--- cron/parse_deposits.php
+++ cron/parse_deposits.php
@@ -1,6 +1,10 @@
 <?php
 require '../util.php';
-$lines = file("bank.csv", FILE_IGNORE_NEW_LINES);
+if (count($argv) < 2) {
+    echo "Need bank CSV\n";
+    exit(-1);
+}
+$lines = file($argv[1], FILE_IGNORE_NEW_LINES);
 
 foreach ($lines as $line_num => $line) {
     $info = split(',', $line);
