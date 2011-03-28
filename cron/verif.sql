37e8abc4839366601069b18b3941c41419655369
genjix <fake@lol.u>
2011-03-28 14:20:44 +0100

    verify payments into the system.

---
+++ cron/verif.sql
@@ -0,0 +1,14 @@
+UPDATE
+    requests
+JOIN
+    purses
+ON
+    purses.uid=requests.uid
+    AND purses.type=requests.curr_type
+SET
+    requests.status='FINAL',
+    purses.amount=requests.amount
+WHERE
+    requests.status='VERIFY'
+    AND requests.timest < NOW() - INTERVAL 1 MINUTE
+;
