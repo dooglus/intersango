fb250085f528bbdbe5a39b8e9a7d73d2e9b9a052
genjix <fake@lol.u>
2011-04-01 13:55:04 +0100

    limit trades to last 24hours.

--- www/api/getTrades.php
+++ www/api/getTrades.php
@@ -24,6 +24,7 @@ $query = "
         transactions.a_orderid=orderbook.orderid
     WHERE
         b_amount >= 0
+        AND timest BETWEEN NOW() - INTERVAL 1 DAY AND NOW()
     ";
 $result = do_query($query);
 $first = true;
