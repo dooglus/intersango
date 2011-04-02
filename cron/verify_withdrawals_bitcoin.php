6b3c8db2b62ed9149d521453702b6483c83f9cb3
genjix <fake@lol.u>
2011-04-02 13:43:59 +0100

    BUGFIX: verif with truncate is too excessive.

--- cron/verify_withdrawals_bitcoin.php
+++ cron/verify_withdrawals_bitcoin.php
@@ -18,7 +18,7 @@ $query = "
     SELECT
         requests.reqid AS reqid,
         uid,
-        TRUNCATE(amount, -8) AS amount,
+        TRUNCATE(amount, -6) AS amount,
         addy
     FROM requests
     JOIN bitcoin_requests
