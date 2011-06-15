e7b98de8d1fd7dcbe994ddf980d5abd14b3cce7b
genjix <fake@lol.u>
2011-06-15 15:23:39 +0100

    When user mistypes deposref, this script allows us to fix that manually.

---
+++ cron/bankd/fix_badref.php
@@ -0,0 +1,65 @@
+<?php
+require '../../util.php';
+
+if (count($argv) < 3) {
+    echo "fix_badref [bank statement ID (bid)] [deposref]\n";
+    exit(-1);
+}
+
+$bid = $argv[1];
+$deposref = $argv[2];
+
+$query = "
+    SELECT entry
+    FROM bank_statement
+    WHERE
+        bid='$bid'
+        AND reqid IS NULL
+        AND status='BADREF'
+    ";
+$result = do_query($query);
+if (!has_results($result)) {
+    echo "No results found for bank statement $bid...\n";
+    exit(-1);
+}
+$row = get_row($result);
+$entry = $row['entry'];
+$entry = split(',', $entry);
+$amount = $entry[6];
+$amount = numstr_to_internal($amount);
+
+$query = "
+    SELECT uid
+    FROM users
+    WHERE deposref='$deposref'
+    ";
+$result = do_query($query);
+if (!has_results($result)) {
+    echo "No user found with deposref of $deposref...\n";
+    exit(-1);
+}
+$row = get_row($result);
+$uid = $row['uid'];
+
+$query = "
+    INSERT INTO requests (
+        req_type,
+        uid,
+        amount,
+        curr_type
+    ) VALUES (
+        'DEPOS',
+        $uid,
+        $amount,
+        'GBP'
+    )";
+do_query($query);
+$query = "
+    UPDATE bank_statement
+    SET
+        reqid=LAST_INSERT_ID(),
+        status='FINAL'
+    ";
+do_query($query);
+echo "Done.\n";
+
