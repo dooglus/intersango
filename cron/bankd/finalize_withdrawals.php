e2390fd026c14dc34402ee27f8708760c0a41346
genjix <fake@lol.u>
2011-06-16 00:10:20 +0100

    Mark withdrawals as finished.

---
+++ cron/bankd/finalize_withdrawals.php
@@ -0,0 +1,13 @@
+<?php
+require '../../util.php';
+
+$query = "
+    UPDATE requests
+    SET status='FINAL'
+    WHERE
+        req_type='WITHDR'
+        AND status='PROCES'
+        AND curr_type='GBP'
+    ";
+do_query($query);
+
