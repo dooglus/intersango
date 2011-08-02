08f69a3f23b68e0939d976b92e37d434e2755e86
Chris Moore <dooglus@gmail.com>
2011-08-02 15:57:12 -0700

    Add function timezone() to allow configuration of timezone used to display times.

--- htdocs/config.php
+++ htdocs/config.php
@@ -6,6 +6,9 @@ enable_errors();
 // how many confirmations we need on incoming bitcoin transfers before adding them to the user accounts
 function confirmations_for_deposit() { return 4; }
 
+// which timezone should we use
+function timezone() { return "Australia/Queensland"; }
+
 // the smallest you can say you 'have' when placing an order
 function minimum_have_amount() { return '0.0005'; }
 
