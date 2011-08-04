37d756c5c08a3d200daa514eef35e2e306406482
Chris Moore <dooglus@gmail.com>
2011-08-04 06:47:19 +0000

    Fix indentation.

--- htdocs/header.php
+++ htdocs/header.php
@@ -3,10 +3,10 @@
 <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
     <title>Intersango Bitcoin Exchange</title>
-    <?php if (!$page) { ?>
+<?php if (!$page) { ?>
     <script type='text/javascript' src='jquery-1.4.4.min.js'></script>
     <script type='text/javascript' src='exchanger.js'></script>
-    <?php 
+<?php 
         require "$abspath/util.php";
         $currencies = array('BTC', 'AUD');
         $rates = array();
