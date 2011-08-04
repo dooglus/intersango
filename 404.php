f058a5276d751909baa21980bddb3af1d29f0c63
Chris Moore <dooglus@gmail.com>
2011-08-04 00:02:39 -0700

    Add 404.php.

---
+++ 404.php
@@ -0,0 +1,10 @@
+<div class='content_box'>
+  <h3>Page Not Found</h3>
+
+  <p>
+    Sorry, but <a href="<?php echo getenv("REQUEST_URI") ?>">that page</a> no longer exists.
+  </p>
+  <p>
+    <center><img width='300' src="/images/tumblbeasts/tb_sign1.png" /><br/><small>thanks to <a target="_blank" href="http://TheOatmeal.com/">Matthew Inman</a> for the image</small></center>
+  </p>
+</div>
