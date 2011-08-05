cd1716f52633432bfb971acf3bfd86854f29641c
Chris Moore <dooglus@gmail.com>
2011-08-05 04:35:39 +0000

    Show commissions in order viewer.

--- view_util.php
+++ view_util.php
@@ -18,14 +18,20 @@ function count_transactions($orderid)
 function display_transactions($uid, $orderid)
 {
     $ordselq = '';
-    if ($orderid != -1)
+    if ($orderid == -1)
+        $sort = "DESC";
+    else {
+        $sort = "ASC";
         $ordselq = " AND orderbook.orderid='$orderid' ";
+    }
     $query = "
         SELECT
             orderbook.orderid AS orderid,
             IF(transactions.a_orderid=orderbook.orderid, 'A', 'B') AS who,
             transactions.a_amount AS a_amount,
             transactions.b_amount AS b_amount,
+            transactions.a_commission AS a_commission,
+            transactions.b_commission AS b_commission,
             orderbook.type AS type,
             orderbook.want_type AS want_type,
             DATE_FORMAT(transactions.timest, '%H:%i %d/%m/%y') AS timest
@@ -35,23 +41,29 @@ function display_transactions($uid, $orderid)
             transactions.a_orderid=orderbook.orderid
             OR transactions.b_orderid=orderbook.orderid
         WHERE orderbook.uid='$uid' $ordselq
-        ORDER BY transactions.txid DESC;
+        ORDER BY transactions.txid $sort;
     ";
     $result = do_query($query);
     $first = true;
     $a_total = 0;
     $b_total = 0;
+    $commission_total = 0;
     $count = 0;
     while ($row = mysql_fetch_assoc($result)) {
         $count++;
         $who = $row['who'];
         $a_amount = $row['a_amount'];
         $b_amount = $row['b_amount'];
-        if ($who == 'B')
+        $a_commission = $row['a_commission'];
+        $b_commission = $row['b_commission'];
+        if ($who == 'B') {
             list($a_amount, $b_amount) = array($b_amount, $a_amount);
+            $b_commission = $a_commission;
+        }
         # skip cancelled orders since we already show those
         if ((int)$b_amount == -1)
             continue;
+
         if ($first) {
             $first = false;
             ?> <div class='content_box'>
@@ -60,6 +72,7 @@ function display_transactions($uid, $orderid)
                 <tr>
                     <th>You gave</th>
                     <th>You got</th>
+                    <th>Commission</th>
                     <th>Effective Price</th>
                     <th>Time</th>
                     <?php if ($orderid == -1) echo '<th></th>'; ?>
@@ -68,8 +81,15 @@ function display_transactions($uid, $orderid)
 
         $a_total += $a_amount;
         $b_total += $b_amount;
+        $commission_total += $b_commission;
+
+        $commission_percent = internal_to_numstr($b_commission) * 100.0 / internal_to_numstr($b_amount);
+
+        $b_amount -= $b_commission;
+
         $a_amount = internal_to_numstr($a_amount);
         $b_amount = internal_to_numstr($b_amount);
+        $b_commission = internal_to_numstr($b_commission);
         $type = $row['type'];
         $want_type = $row['want_type'];
         if ($type == 'BTC')
@@ -80,7 +100,8 @@ function display_transactions($uid, $orderid)
         $this_orderid = $row['orderid'];
         $timest = $row['timest'];
         echo "    <tr>\n";
-        echo "        <td>$a_amount $type</td><td>$b_amount $want_type</td><td>$price</td>\n";
+        echo "        <td>$a_amount $type</td><td>$b_amount $want_type</td><td>$b_commission $want_type (",
+            sprintf("%.3f", $commission_percent), "%)</td><td>$price</td>\n";
         echo "        <td>$timest</td>\n";
         if ($orderid == -1)
             echo "        <td><a href='?page=view_order&orderid=$this_orderid'>View</a></td>\n";
@@ -88,8 +109,12 @@ function display_transactions($uid, $orderid)
     }
     if (!$first) {
         if ($orderid != -1 && $count > 1) {
+            $commission_percent = internal_to_numstr($commission_total) * 100.0 / internal_to_numstr($b_total);
+
+            $b_total -= $commission_total;
             $a_total = internal_to_numstr($a_total);
             $b_total = internal_to_numstr($b_total);
+            $commission_total = internal_to_numstr($commission_total);
 
             if ($type == 'BTC')
                 $price = $b_total / $a_total;
@@ -98,11 +123,12 @@ function display_transactions($uid, $orderid)
             $price = sprintf("%.6f", $price);
             
             echo "    <tr>\n";
-            echo "        <td>--------</td><td>--------</td><td>--------</td>\n";
+            echo "        <td>--------</td><td>--------</td><td>--------</td><td>--------</td>\n";
             echo "        <td></td>\n";
             echo "    </tr>\n";
             echo "    <tr>\n";
-            echo "        <td>$a_total $type</td><td>$b_total $want_type</td><td>$price</td>\n";
+            echo "        <td>$a_total $type</td><td>$b_total $want_type</td><td>$commission_total $want_type (",
+                sprintf("%.3f", $commission_percent), "%)</td><td>$price</td>\n";
             echo "        <td></td>\n";
             echo "    </tr>\n";
         }
