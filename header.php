0381321cc8019000d98272904aaee22f9bf3e145
Chris Moore <dooglus@gmail.com>
2011-08-07 08:55:27 +0000

    Remove 'Intersango' from the title.

--- header.php
+++ header.php
@@ -7,7 +7,7 @@ function show_header($page)
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-    <title>Intersango Bitcoin Exchange</title>
+    <title>World Bitcoin Exchange</title>
 <?php if ($page == 'trade') { ?>
     <script type='text/javascript' src='js/jquery-1.4.4.min.js'></script>
     <script type='text/javascript' src='js/exchanger.js'></script>
