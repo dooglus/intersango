4b64a897e2551b46574ed2bc217eb5c7973bef09
Chris Moore <dooglus@gmail.com>
2011-08-07 04:32:00 -0700

    More tests.

--- test.php
+++ test.php
@@ -4,13 +4,19 @@ require_once "util.php";
 function test_aud_commission($aud, $already_paid = '0')
 {
     $commission = commission_on_aud(numstr_to_internal($aud), numstr_to_internal($already_paid));
-    echo "<li>commission selling BTC for <b>$aud</b> AUD is <b>", internal_to_numstr($commission), "</b> AUD if $already_paid was already paid<br/>\n";
+    echo "<li>commission selling BTC for <b>$aud</b> AUD is <b>", internal_to_numstr($commission), "</b> AUD";
+    if ($already_paid)
+        echo " if $already_paid was already paid";
+    echo "<br/>\n";
 }
 
 function test_btc_commission($btc, $already_paid = '0')
 {
     $commission = commission_on_btc(numstr_to_internal($btc), numstr_to_internal($already_paid));
-    echo "<li>commission buying <b>$btc</b> BTC is <b>", internal_to_numstr($commission), "</b> AUD if $already_paid was already paid<br/>\n";
+    echo "<li>commission buying <b>$btc</b> BTC is <b>", internal_to_numstr($commission), "</b> AUD";
+    if ($already_paid)
+        echo " if $already_paid was already paid";
+    echo "<br/>\n";
 }
 
 echo "<div class='content_box'>\n";
@@ -23,19 +29,41 @@ echo "<h3>Commission buying BTC</h3>\n";
 echo "<p>rate is ", commission_percentage_for_btc(), "%",
     " and cap is ", commission_cap_in_btc(), " BTC</p>\n";
 echo "<ul>\n";
-test_btc_commission(.0001, '1');
-test_btc_commission(.0001, '0.012');
-test_btc_commission(.0001);
-test_btc_commission(.001);
-test_btc_commission(.01);
-test_btc_commission(.1);
-test_btc_commission(1);
-test_btc_commission(10);
-test_btc_commission(100);
-test_btc_commission(1000);
-test_btc_commission(10000);
-test_btc_commission(10000, '0.012');
-test_btc_commission(10000, '1');
+test_btc_commission('0.000000001');
+test_btc_commission('0.00000001');
+test_btc_commission('0.0000001');
+test_btc_commission('0.000001');
+test_btc_commission('0.00001');
+test_btc_commission('0.0001', '1');
+test_btc_commission('0.0001', '0.012');
+test_btc_commission('0.0001');
+test_btc_commission('0.001');
+test_btc_commission('0.01');
+test_btc_commission('0.1');
+test_btc_commission('1');
+test_btc_commission('10');
+test_btc_commission('100');
+test_btc_commission('1000');
+test_btc_commission('10000');
+test_btc_commission('10000', '0.012');
+test_btc_commission('10000', '1');
+test_btc_commission('100000');
+test_btc_commission('1000000');
+test_btc_commission('10000000');
+test_btc_commission('100000000');
+test_btc_commission('1000000000');
+test_btc_commission('10000000000');
+test_btc_commission('100000000000');
+test_btc_commission('1000000000000');
+test_btc_commission('10000000000000');
+test_btc_commission('100000000000000');
+test_btc_commission('1000000000000000');
+test_btc_commission('10000000000000000');
+test_btc_commission('100000000000000000');
+test_btc_commission('1000000000000000000');
+test_btc_commission('10000000000000000000');
+test_btc_commission('100000000000000000000');
+test_btc_commission('1000000000000000000000');
 echo "</ul></div>\n";
 
 echo "<div class='content_box'>\n";
@@ -43,18 +71,21 @@ echo "<h3>Commission selling BTC</h3>\n";
 echo "<p>rate is ", commission_percentage_for_aud(), "%",
     " and cap is ", commission_cap_in_aud(), " AUD</p>\n";
 echo "<ul>\n";
-test_aud_commission(.0001, '1');
-test_aud_commission(.0001, '0.012');
-test_aud_commission(.0001);
-test_aud_commission(.001);
-test_aud_commission(.01);
-test_aud_commission(.1);
-test_aud_commission(1);
-test_aud_commission(10);
-test_aud_commission(100);
-test_aud_commission(1000);
-test_aud_commission(10000);
-test_aud_commission(10000, '0.012');
-test_aud_commission(10000, '1');
+test_aud_commission('0.0001', '1');
+test_aud_commission('0.0001', '0.012');
+test_aud_commission('0.0001');
+test_aud_commission('0.001');
+test_aud_commission('0.01');
+test_aud_commission('0.1');
+test_aud_commission('1');
+test_aud_commission('10');
+test_aud_commission('100');
+test_aud_commission('1000');
+test_aud_commission('10000');
+test_aud_commission('10000', '0.012');
+test_aud_commission('10000', '1');
+test_aud_commission('100000');
+test_aud_commission('1000000');
+test_aud_commission('10000000');
 echo "</ul></div>\n";
 ?>
