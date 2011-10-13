47b33f52c88aee2abe1aeac07be2effd135ff241
Chris Moore <dooglus@gmail.com>
2011-10-12 18:33:35 -0700

    Initial revision.

---
+++ news.php
@@ -0,0 +1,18 @@
+<div class='content_box'>
+<h3>Better Matching Code</h3>
+<h4>11:17pm 13-Oct-11 </h4>
+<p>
+     I just made a tweak to the matching algorithm.  If you ever clicked on the best available offer on the trade page and submitted it only to find that your order just sat in the orderbook never being matched, this is for you.  
+</p>
+<p>
+     The tweak changes order amounts on partial matches by no more than one hundred millionth of a bitcoin but will stop this from happening in the future.  Existing existing partial orders on the orderbook from before this fix will still show the old behaviour, but once they are bought up everything will be find.  Until then, you can always just offer a slightly better price than the order you're trying to match - then it will match.
+</p>
+</div>
+
+<div class='content_box'>
+<h3>Google +1 Button</h3>
+<h4>9:42pm 12-Oct-11 </h4>
+<p>
+     There's a new "+1" button under the links in the right-hand panel for Google+ users.  It allows you to recommend the exchange to your Google contacts.
+</p>
+</div>
