41ee91ba9252d24d1bcfb645702d90f8b0bb73b9
Chris Moore <dooglus@gmail.com>
2011-11-14 04:50:15 -0800

    This is the first API call needing the user to provide key and secret.

---
+++ htdocs/api/withdraw_voucher.php
@@ -0,0 +1,21 @@
+<?php
+require_once "../config.php";
+require_once ABSPATH . "/withdraw_utils.php";
+
+function main()
+{
+    global $is_logged_in;
+
+    $_POST['is_international'] = 0;
+    $_POST['voucher'] = 1;
+
+    do_withdraw(post('amount'), post('currency'), $voucher_code, $reqid);
+    
+    return array("status"  => "OK",
+                 "voucher" => $voucher_code,
+                 "reqid"   => $reqid);
+}
+
+process_api_request("main", "withdraw");
+
+?>
