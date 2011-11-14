6134725133471628484dde42c09f6dae48057c4f
Chris Moore <dooglus@gmail.com>
2011-11-14 08:02:30 -0800

    New API call to get user balance, uid, etc.

---
+++ htdocs/api/info.php
@@ -0,0 +1,19 @@
+<?php
+require_once "../config.php";
+require_once ABSPATH . "/util.php";
+
+function main()
+{
+    global $is_logged_in;
+
+    $balances = fetch_balances($is_logged_in);
+    
+    return array("status"  => "OK",
+                 "uid"     => $is_logged_in,
+                 "BTC"     => internal_to_numstr($balances['BTC']),
+                 CURRENCY  => internal_to_numstr($balances[CURRENCY]));
+}
+
+process_api_request("main", "read");
+
+?>
