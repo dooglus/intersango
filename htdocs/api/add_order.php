9092bdce86a45071df02e429823325d86836718a
Chris Moore <dooglus@gmail.com>
2011-11-14 22:53:02 -0800

    New API.

---
+++ htdocs/api/add_order.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../config.php";
+require_once ABSPATH . "/order_utils.php";
+
+function main()
+{
+    $reqid = place_order(post('have_amount'), post('have_currency'),
+                         post('want_amount'), post('want_currency'));
+
+    return array("status"  => "OK",
+                 "orderid" => $reqid);
+}
+
+process_api_request("main", "trade");
+
+?>
