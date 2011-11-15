a04590586d99ca5cc91a17eb4057e47bd206aa14
Chris Moore <dooglus@gmail.com>
2011-11-14 22:56:03 -0800

    Move 'place_order' code out into order_utils.php for new API call.

--- place_order.php
+++ place_order.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'util.php';
+require_once 'order_utils.php';
 require_once "errors.php";
 
 if(isset($_POST['csrf_token']))
@@ -14,62 +14,24 @@ else
     throw new Error("csrf","csrf token missing");
 }
 
-$uid = user_id();
+$have_amount   = post('amount');
+$have_currency = post('type');
+$want_amount   = post('want_amount');
+$want_currency = post('want_type');
 
-$type = post('type');
-$type = strtoupper($type);
-$want_type = post('want_type');
-$want_type = strtoupper($want_type);
+$_SESSION['currency_in'] = strtoupper($have_currency);
 
-$_SESSION['currency_in'] = $type;
+$orderid = place_order($have_amount, $have_currency, $want_amount, $want_currency);
 
-curr_supported_check($type);
-curr_supported_check($want_type);
-
-// convert for inclusion into database
-$amount_disp = post('amount');
-$amount = numstr_to_internal($amount_disp);
-$want_amount_disp = post('want_amount');
-$want_amount = numstr_to_internal($want_amount_disp);
-
-order_worthwhile_check($amount, $amount_disp, MINIMUM_HAVE_AMOUNT);
-order_worthwhile_check($want_amount, $want_amount_disp, MINIMUM_WANT_AMOUNT);
-
-enough_money_check($amount, $type);
-
-do_query("START TRANSACTION");
-// deduct money from their account
-deduct_funds($amount, $type);
-
-// add the money to the order book
-$query = "
-    INSERT INTO orderbook (
-        uid,
-        initial_amount,
-        amount,
-        type,
-        initial_want_amount,
-        want_amount,
-        want_type)
-    VALUES (
-        '$uid',
-        '$amount',
-        '$amount',
-        '$type',
-        '$want_amount',
-        '$want_amount',
-        '$want_type');
-    ";
-$result = do_query($query);
-$orderid = mysql_insert_id();
-do_query("COMMIT");
 ?>
 
 <div class='content_box'>
     <h3><?php echo _("Order placed"); ?></h3>
+
     <p><?php printf(_("Your order offering %s for %s has been placed."),
-                    $amount_disp . " " . $type,
-                    $want_amount_disp . " " . $want_type); ?></p>
+                    $have_amount . " " . $have_currency,
+                    $want_amount . " " . $want_currency); ?></p>
+
     <p><?php printf(_("You may view your %snew order%s or visit the %sorderbook%s."),
                     '<a href="?page=view_order&orderid=' . $orderid . '">',
                     '</a>',
