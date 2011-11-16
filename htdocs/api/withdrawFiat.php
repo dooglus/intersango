89956613e8749fdc7ed8ceb00c8cdf0bb50b6218
Chris Moore <dooglus@gmail.com>
2011-11-15 16:54:35 -0800

    Currency is passed as parameter, not as a POST variable.

--- htdocs/api/withdrawFiat.php
+++ htdocs/api/withdrawFiat.php
@@ -6,9 +6,8 @@ function main()
 {
     $_POST['is_international'] = 0;
     unset($_POST['voucher']);
-    $_POST['currency'] = CURRENCY;
 
-    do_withdraw(post('amount'), post('currency'), $voucher_code, $reqid);
+    do_withdraw(post('amount'), CURRENCY, $voucher_code, $reqid);
     
     return array("status"  => "OK",
                  "reqid"   => $reqid);
