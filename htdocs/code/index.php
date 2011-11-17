8e33217972150e35165e4bf4783a79bc1b096e39
Chris Moore <dooglus@gmail.com>
2011-11-17 01:28:49 -0800

    Add index file.

---
+++ htdocs/code/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+ <head>
+  <title>World Bitcoin Exchange API Code</title>
+ </head>
+ <body>
+<h1>World Bitcoin Exchange API Code</h1>
+<ul>
+<li><a href="wbx_api.php"> wbx_api.php</a> -- defines the WBX_API class which documents and provides methods for each API call</li>
+<li><a href="demo.php"> demo.php</a> -- a simple example of how to use the WBX_API class</li>
+</ul>
+</body></html>
