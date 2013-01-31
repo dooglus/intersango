72fb9a835f5d9bc244c64f901a447a2d9e61a115
Chris Moore <dooglus@gmail.com>
2013-01-31 10:00:14 -0800

    Initial revision.

---
+++ contact.php
@@ -0,0 +1,4 @@
+<div class='content_box'>
+<h3>Contact Us</h3>
+<p>If you need to contact me about anything, I can be reached via email at <img style='position: relative; top:2px;' src="images/email.png"> or on Skype as 'dooglus'.</p>
+</div>
